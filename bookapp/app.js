801edd6ca1dc7c499d455aeacf19ae64cbe8ea5d
Paul <jamespauloconnor@gmail.com>
2016-07-09 15:42:20 -0400

    Initial commit. Template up and running

---
+++ bookapp/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+
+var app = express();
+var port = 5000;
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(request, response){
+	response.send("Hello World");
+});
+
+app.get('/books', function(request, response){
+	response.send("Hello books");
+});
+
+app.listen(5000, function(err){
+	console.log('Runnong on port' + port);
+});
